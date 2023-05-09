96ac389c93042de533f8d4187de59625d4342977
[alyanamacedo] <[alyanamacedo@hotmail.com]>
2023-05-09 09:35:40 -0300

    landingpage ainda com alguns erros, pendente de responsividade, correções de acessibilidade

--- projetoFinal/atividadeScript.js
+++
@@ -1,9 +0,0 @@
-// Add active class to the current button (highlight it)
-var btns = document.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace("active", "");
-    this.className += " active";
-  });
-}
